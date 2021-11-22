ae89ac5b36672e0aedbe52836e3e6763f5a8722f
a17labs <*a17matte@gmail.com*>
2021-11-22 19:12:37 +0000

    changes

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Chat from './components/Chat';
+import Start from './components/Start';
+
+// import react native gesture handler
+import 'react-native-gesture-handler';
+
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Create the navigator
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Chatbox"
+      >
+        <Stack.Screen
+          name="Chatbox"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/Start.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from "react-native";
+
+const image = require("../assets/icon.jpg");
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    backgroundImage: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+      },
+  });
+  
+
+export default class Start extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "" };
+    }
+
+    render() {
+        return(
+           
+           
+            <ImageBackground source={image} style={styles.backgroundImage} >
+               <View style={styles.container}>
+                <Text style={{ color: "#Black", fontSize: 15, fontWeight: 700 }}>What's the topic?</Text>
+                    <TextInput 
+                     style={{ height: 40, borderColor: "grey", borderWidth: 1, backgroundColor: "white", padding: 10,  }}
+                     onChangeText={(name) => this.setState({ name })}
+                     value={this.state.name}
+                     placeholder="Chat here"
+                     />
+                    <Button
+                        title="Package"
+                        onPress={() => 
+                            this.props.navigation.navigate("Chat", { name: this.state.name })
+                         }  style={{ color: "#000", backgroundColor: "#fff" }}
+                    />
+                   </View>
+                 </ImageBackground>
+           
+            
+        )
+    }
+    
+}
