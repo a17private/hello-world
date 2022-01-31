e92ab36189a66cba086e1e34c075281ee085e19a
a17labs <*a17matte@gmail.com*>
2022-01-31 18:13:50 +0000

    changes

--- components/Chat.js
+++ components/Chat.js
@@ -32,7 +32,7 @@ export default class Chat extends React.Component {
         _id: '',
         name: '',
       },
-      isConnected: true, 
+      isConnected: false, 
       image: null,
       location: null
   };
