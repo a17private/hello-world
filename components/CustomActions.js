a85d5e4aa33fd287e22384be8688d92381da4447
a17labs <*a17matte@gmail.com*>
2022-02-12 22:15:23 +0000

    chanegs

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import * as Permisions from "expo-permissions";
 import * as Location from "expo-location";
 import firebase from "firebase";
 import "firebase/firestore";
